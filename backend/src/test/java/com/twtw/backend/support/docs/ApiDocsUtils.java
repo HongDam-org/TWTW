dd0e7f2a7aac6e911ad97e2dc56bb03e2f944708
김승진 <ohksj77@gmail.com>
2023-08-09 08:15:30 +0900

    [FEAT] rest docs 세팅

---
+++ backend/src/test/java/com/twtw/backend/support/docs/ApiDocsUtils.java
@@ -0,0 +1,18 @@
+package com.twtw.backend.support.docs;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+public interface ApiDocsUtils {
+
+    static OperationRequestPreprocessor getDocumentRequest() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getDocumentResponse() {
+        return preprocessResponse(prettyPrint());
+    }
+}
