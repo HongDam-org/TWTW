bcfdf3993d91c515032812f47a059c837ddd29f6
김승진 <ohksj77@gmail.com>
2023-08-17 01:55:27 +0900

    chore: 테스트용 인증 제거 부분 복구

--- backend/src/main/java/com/twtw/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/twtw/backend/config/security/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig {
                                                 "/v3/api-docs/**",
                                                 "/swagger-ui/**",
                                                 "/swagger-ui.html",
-                                                "auth/**",
-                                                "/plans/**")
+                                                "auth/**")
                                         .permitAll())
                 .authorizeHttpRequests(
                         x -> x.requestMatchers("/test/**").permitAll().anyRequest().authenticated())
