21a79728e4634bcde5420f88c40791ba8fd6eb49
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-08-31 22:55:48 +0900

    [ADD] Naver Maps Api ResultType 완성 + Config , ObjectMapper 분리

--- backend/src/main/java/com/twtw/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/twtw/backend/config/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                                                 "/v3/api-docs/**",
                                                 "/swagger-ui/**",
                                                 "/swagger-ui.html",
-                                                "auth/**","/paths/**","/plans/**")
+                                                "auth/**")
                                         .permitAll())
                 .authorizeHttpRequests(
                         x -> x.requestMatchers("/test/**").permitAll().anyRequest().authenticated())
