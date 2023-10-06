7a29d80f86259a68ee9d69f3b8bdbf7b5707d6c0
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-10-06 09:41:50 +0900

    [ADD] Member Duplicate

--- backend/src/main/java/com/twtw/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/twtw/backend/domain/member/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.twtw.backend.domain.member.controller;
 
+import com.twtw.backend.domain.member.dto.response.DuplicateNicknameDto;
 import com.twtw.backend.domain.member.service.MemberService;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,10 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/duplicate/{name}")
+    public ResponseEntity<DuplicateNicknameDto> duplicateNickname(@PathVariable String name){
+        return ResponseEntity.ok(memberService.duplicateNickname(name));
+    }
+
 }
