ed80202208a6c0dc67e4106691c462ce7205fd54
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-10-09 10:53:41 +0900

    [ADD] Group Controller 기본 작업

--- backend/src/main/java/com/twtw/backend/domain/group/controller/GroupController.java
+++ backend/src/main/java/com/twtw/backend/domain/group/controller/GroupController.java
@@ -1,16 +1,25 @@
 package com.twtw.backend.domain.group.controller;
 
+import com.twtw.backend.domain.group.dto.GroupInfoDto;
 import com.twtw.backend.domain.group.service.GroupService;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/group")
 public class GroupController {
     private final GroupService groupService;
-
     public GroupController(GroupService groupService) {
         this.groupService = groupService;
     }
+
+    //자신이 속한 그룹 반환 API
+    @GetMapping("/get/{id}")
+    public void getMyGroups(@PathVariable String id){}
 }
