8479509fb01528f06c97d686d88e9474e944305b
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-11-06 18:07:58 +0900

    [FEAT] Map Struct 1차 리팩토링

--- backend/src/main/java/com/twtw/backend/domain/place/mapper/PlaceMapper2.java
+++ backend/src/main/java/com/twtw/backend/domain/place/mapper/PlaceMapper2.java
@@ -3,11 +3,24 @@ package com.twtw.backend.domain.place.mapper;
 import com.twtw.backend.domain.place.entity.Place;
 import com.twtw.backend.domain.plan.dto.client.PlaceDetails;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PlaceMapper2 {
+    @Mapping(target ="distance",source = "distance",qualifiedByName = "toInteger")
     Place toEntity(PlaceDetails detail);
-
+    @Mapping(target ="distance",source = "distance",qualifiedByName = "toString")
     PlaceDetails toPlaceResponse(Place place);
+
+    @Named("toInteger")
+    default Integer convertInteger(String distance){
+        return Integer.parseInt(distance);
+    }
+
+    @Named("toString")
+    default String convertString(Integer distance){
+        return String.valueOf(distance);
+    }
 }
