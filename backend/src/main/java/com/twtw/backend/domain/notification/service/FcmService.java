ef7bddc8cdd9a8a80ea051a5337b14925a2af04a
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2023-12-23 16:46:29 +0900

    [FEAT] FcmService Logic

--- backend/src/main/java/com/twtw/backend/domain/notification/service/FcmService.java
+++ backend/src/main/java/com/twtw/backend/domain/notification/service/FcmService.java
@@ -4,16 +4,16 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.twtw.backend.domain.notification.dto.NotificationRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class FcmService {
-
-    @Autowired
     private final FirebaseMessaging firebaseMessaging;
+
+    public FcmService(FirebaseMessaging firebaseMessaging){
+        this.firebaseMessaging = firebaseMessaging;
+    }
 
     public void sendNotification(NotificationRequest request) throws FirebaseMessagingException {
         Message message = Message.builder()
