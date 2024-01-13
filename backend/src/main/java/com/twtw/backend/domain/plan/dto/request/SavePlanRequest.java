5fd23c5a9f055c1d9cba3b14bd1bc22fe22e5cad
김승진 <ohksj77@gmail.com>
2024-01-13 20:34:39 +0900

    [HOTFIX] localdatetime 수정 완료

--- backend/src/main/java/com/twtw/backend/domain/plan/dto/request/SavePlanRequest.java
+++ backend/src/main/java/com/twtw/backend/domain/plan/dto/request/SavePlanRequest.java
@@ -1,11 +1,10 @@
 package com.twtw.backend.domain.plan.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.twtw.backend.domain.plan.dto.response.PlaceDetails;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
@@ -18,11 +17,9 @@ import java.util.UUID;
 public class SavePlanRequest {
     private String name;
     private UUID groupId;
-
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime planDay;
-
     private PlaceDetails placeDetails;
-
     private List<UUID> memberIds;
 }
