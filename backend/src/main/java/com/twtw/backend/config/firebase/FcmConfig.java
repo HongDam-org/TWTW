ba919527c744c0c1e4acdbd99e35c55d74ca73b7
김승진 <ohksj77@gmail.com>
2024-03-08 22:08:30 +0900

    [FIX] fixing fcm config

--- backend/src/main/java/com/twtw/backend/config/firebase/FcmConfig.java
+++ backend/src/main/java/com/twtw/backend/config/firebase/FcmConfig.java
@@ -6,31 +6,36 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.twtw.backend.global.properties.FirebaseProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class FcmConfig {
 
     private final FirebaseProperties firebaseProperties;
-    private final ApplicationContext context;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
+        log.info("FirebaseApp initializing...11111");
+
         final FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(
-                        context.getResource(firebaseProperties.getLocation()).getInputStream()))
+                        new ClassPathResource(firebaseProperties.getLocation()).getInputStream()))
                 .build();
+        log.info("FirebaseApp initializing...22222");
 
         if (FirebaseApp.getApps().isEmpty()) {
+            log.info("FirebaseApp initializing...33333");
             return FirebaseApp.initializeApp(options);
-        } else {
-            return FirebaseApp.getInstance();
         }
+        log.info("FirebaseApp initializing...44444");
+        return FirebaseApp.getInstance();
     }
 
     @Bean
