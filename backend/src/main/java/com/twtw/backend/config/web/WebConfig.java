411cc3fb7045d09ffa9b3ef3add9723c14b57434
김승진 <ohksj77@gmail.com>
2024-03-08 22:49:39 +0900

    [FIX] objectmapper to parse json with value containing backslash

--- backend/src/main/java/com/twtw/backend/config/web/WebConfig.java
+++ backend/src/main/java/com/twtw/backend/config/web/WebConfig.java
@@ -1,12 +1,14 @@
 package com.twtw.backend.config.web;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -23,11 +25,14 @@ public class WebConfig extends WebMvcConfigurationSupport {
         super.configureMessageConverters(converters);
     }
 
-    private ObjectMapper objectMapper() {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+                .configure(JsonParser.Feature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER, true)
                 .setPropertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
                 .registerModule(new JavaTimeModule());
     }
