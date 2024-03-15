8eeaf0d1b2b5ad0e9202c1c72f5d94b0b086bdd2
김승진 <ohksj77@gmail.com>
2024-03-15 21:38:47 +0900

    [FIX] filter 발생 예외 controller advice 에서 처리

--- backend/src/main/java/com/twtw/backend/config/security/jwt/JwtAccessDeniedHandler.java
+++ backend/src/main/java/com/twtw/backend/config/security/jwt/JwtAccessDeniedHandler.java
@@ -4,20 +4,30 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final HandlerExceptionResolver resolver;
+
+    public JwtAccessDeniedHandler(
+            @Qualifier("handlerExceptionResolver") final HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
     public void handle(
             HttpServletRequest request,
             HttpServletResponse response,
             AccessDeniedException accessDeniedException)
             throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        resolver.resolveException(request, response, null, accessDeniedException);
     }
 }
--- backend/src/main/java/com/twtw/backend/config/security/jwt/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/twtw/backend/config/security/jwt/JwtAuthenticationEntryPoint.java
@@ -4,21 +4,29 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final HandlerExceptionResolver resolver;
+
+    public JwtAuthenticationEntryPoint(
+            @Qualifier("handlerExceptionResolver") final HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
     public void commence(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            AuthenticationException authException)
-            throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final AuthenticationException authException) throws IOException, ServletException {
+        resolver.resolveException(request, response, null, authException);
     }
 }
