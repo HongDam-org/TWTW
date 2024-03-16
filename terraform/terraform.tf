75f24c2ed26738fb10953cc128d1823b03d0a4ca
김승진 <ohksj77@gmail.com>
2024-03-16 21:50:41 +0900

    [FEAT] nginx stomp connection ttl

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,6 +1,5 @@
 resource "aws_default_vpc" "vpc_network" {
   tags                 = merge(var.tags, {})
-  cidr_block           = "10.0.0.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
